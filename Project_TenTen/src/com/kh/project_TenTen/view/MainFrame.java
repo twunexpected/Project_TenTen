c4831946588b2cb1fe55c58e498900f4c79245c8
uzdev <uzdev@218.38.137.28>
2019-11-15 20:43:14 +0900

    YaHooooooo~~~~~~~~~~

--- Project_TenTen/src/com/kh/project_TenTen/view/MainFrame.java
+++ Project_TenTen/src/com/kh/project_TenTen/view/MainFrame.java
@@ -3,12 +3,16 @@ package com.kh.project_TenTen.view;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
-	public MainFrame(){
-		this.setBounds(650, 100, 400, 700);
-		this.setLayout(null);
-		new MyWord1_DJ(this);
+	public MainFrame() {
+		
+		this.setSize(400,700);
+		
+		new Main_UZ(this);
+		
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 	}
-
+	
 }
--- Project_TenTen/src/com/kh/project_TenTen/view/Main_UZ.java
+++ Project_TenTen/src/com/kh/project_TenTen/view/Main_UZ.java
@@ -3,77 +3,77 @@ package com.kh.project_TenTen.view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Main_UZ extends JFrame {
+public class Main_UZ extends JPanel {
 
-	public Main_UZ() {
+	private MainFrame mf;
+	private JPanel Main_UZ;
+
+	public Main_UZ(MainFrame mf) {
+		this.mf = mf;
+		Main_UZ = this;
+		
 		// 프레임 
-		super("텐텐 메인 페이지");
-		this.setBounds(650, 100, 400, 700);
-		this.setLayout(null);
+//		super("텐텐 메인 페이지");
+//		this.setBounds(650, 100, 400, 700);
+//		this.setLayout(null);
 
 
 
 		//마이페이지 버튼
-		JPanel MyPage = new JPanel();
-		MyPage.setBackground(new Color(123, 185, 237));
-		MyPage.setSize(120, 45);
-		MyPage.setLocation(260, 20);
 		JButton btn = new JButton("마이페이지");
+		btn.setSize(120, 45);
+		btn.setLocation(260, 20);
+		btn.setForeground(Color.WHITE);
 		btn.setBackground(new Color(36, 107, 220));
 		btn.setFont(new Font("고딕",Font.PLAIN,12));
 		btn.setPreferredSize(new Dimension(120, 45));
 
-
+		
+		
 
 		//암기 버튼
-		JPanel	memory1 = new JPanel();
-		memory1.setBackground(new Color(123, 185, 237));
-		memory1.setSize(105, 60);
-		memory1.setLocation(105,390);
 		JButton arm = new JButton("암기");
+		arm.setSize(105, 60);
+		arm.setLocation(105,390);
+		arm.setForeground(Color.WHITE);
 		arm.setPreferredSize(new Dimension(105, 60));
 		arm.setBackground(new Color(36, 107, 220));
 
 
-		//비암기버
-		JPanel	memory2 = new JPanel();
-		memory2.setBackground(new Color(123, 185, 237));
-		memory2.setSize(105, 60);
-		memory2.setLocation(240, 390);
+		//비암기버튼
 		JButton beearm = new JButton("비암기");
+		beearm.setSize(105, 60);
+		beearm.setLocation(240, 390);
+		beearm.setForeground(Color.WHITE);
 		beearm.setPreferredSize(new Dimension(105, 60));
 		beearm.setBackground(new Color(36, 107, 220));
 
 
 
 		//주제 여 텍스
-		JPanel sectionPanel = new JPanel();
-		sectionPanel.setBackground(new Color(123, 185, 237));
-		//sectionPanel.setBounds(130, 100, 200, 200);
-		sectionPanel.setSize(80, 50);
-		sectionPanel.setLocation(185, 120);
 		JTextField sectionText = new JTextField("  주제");
+		sectionText.setSize(80, 50);
+		sectionText.setLocation(185, 120);
 		sectionText.setBackground(new Color(123, 185, 237));
 		sectionText.setFont(new Font("고딕",Font.PLAIN,20));
 		sectionText.setPreferredSize(new Dimension(80, 50));
 
 
 		//단어 텍스
-		JPanel wordView = new JPanel();
-		//wordView.setBounds(20, 200, 500, 500);
-		wordView.setBackground(new Color(123, 185, 237));
-		wordView.setSize(240, 90);
-		wordView.setLocation(105, 180);
+//		wordView.setBackground(new Color(123, 185, 237));
 		JTextField wordText = new JTextField(" 단 어");
+		wordText.setSize(240, 90);
+		wordText.setLocation(105, 180);
 		wordText.setBackground(new Color(123, 185, 237));
 		wordText.setFont(new Font("고딕",Font.BOLD,80));
 		wordText.setPreferredSize(new Dimension(240, 90));
@@ -81,40 +81,31 @@ public class Main_UZ extends JFrame {
 
 
 		//카운 텍스
-		JPanel counting = new JPanel();
-		counting.setBackground(new Color(123, 185, 237));
-		counting.setSize(50, 40);
-		counting.setLocation(170, 460);
 		JTextField countnum = new JTextField("입력값");
+		countnum.setBackground(new Color(123, 185, 237));
+		countnum.setSize(60, 50);
+		countnum.setLocation(165, 460);
 		countnum.setFont(new Font("고딕",Font.PLAIN,15));
-		countnum.setPreferredSize(new Dimension(50, 40));
+		countnum.setPreferredSize(new Dimension(60, 50));
 
 
 		// " /10 "
-		JPanel countingMax = new JPanel();
-		countingMax.setBackground(new Color(123, 185, 237));
-		countingMax.setSize(50, 40);
-		countingMax.setLocation(220, 465);
 		JLabel countMax = new JLabel("/ 10");
+		countMax.setBackground(new Color(123, 185, 237));
+		countMax.setSize(50, 40);
+		countMax.setLocation(225, 465);
 		countMax.setFont(new Font("고딕",Font.PLAIN,20));
 		//	라벨은 사이즈값 따로 안써도 패널에 자동 부착.
 
 
 
-		countingMax.add(countMax);
-		this.add(countingMax);
-		MyPage.add(btn);
-		this.add(MyPage);
-		memory1.add(arm);
-		this.add(memory1);
-		memory2.add(beearm);
-		this.add(memory2);
-		sectionPanel.add(sectionText);
-		this.add(sectionPanel);
-		wordView.add(wordText);
-		this.add(wordView);
-		counting.add(countnum);
-		this.add(counting);
+		this.add(countMax);
+		this.add(btn);
+		this.add(arm);
+		this.add(beearm);
+		this.add(sectionText);
+		this.add(wordText);
+		this.add(countnum);
 
 
 		//카피라잇
@@ -127,14 +118,18 @@ public class Main_UZ extends JFrame {
 		
 		//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 		//배경
-		JPanel jp = new JPanel();
-		jp.setSize(400, 700);
-		jp.setLayout(null);
-		jp.setBackground(new Color(123, 185, 237));
-		this.add(jp);   
-		this.setBounds(600, 100, 400, 700);
+//		JPanel jp = new JPanel();
+//		jp.setSize(400, 700);
+//		jp.setLayout(null);
+//		jp.setBackground(new Color(123, 185, 237));
+//		mf.add(this);   
+//		this.setBounds(600, 100, 400, 700);
 		
-
+		this.setSize(400, 700);
+		this.setLayout(null);
+		this.setBackground(new Color(123, 185, 237));
+		mf.add(this);   
+		
 		//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 		//좌측 메뉴
 		final JButton btnLeft1 = new JButton("<html>메인<br/>화면</html>");
@@ -142,23 +137,23 @@ public class Main_UZ extends JFrame {
 		btnLeft1.setForeground(new Color(255, 255, 225));
 		btnLeft1.setBackground(new Color(36, 107, 220));
 		btnLeft1.setFont(new Font("고딕",Font.BOLD,20));
-		jp.add(btnLeft1);
+		this.add(btnLeft1);
 		JPanel panelLeft1 = new JPanel();
 		panelLeft1.setBounds(0,120, 80, 10);
 		panelLeft1.setBackground(new Color(255, 255, 255));
 		this.setLayout(null);
-		jp.add(panelLeft1);
+		this.add(panelLeft1);
 
 		final JButton btnLeft2 = new JButton("<html>나의<br/>단어</html>");
 		btnLeft2.setBounds(0, 130, 80, 120);
 		btnLeft2.setForeground(new Color(255, 255, 225));
 		btnLeft2.setBackground(new Color(36, 107, 220));
 		btnLeft2.setFont(new Font("고딕",Font.BOLD,20));
-		jp.add(btnLeft2);
+		this.add(btnLeft2);
 		JPanel panelLeft2= new JPanel();
 		panelLeft2.setBounds(0, 250, 80, 10);
 		panelLeft2.setBackground(new Color(255, 255, 255));
-		jp.add(panelLeft2);
+		this.add(panelLeft2);
 
 
 		final JButton btnLeft3 = new JButton("<html>단어<br/>추가</html>");
@@ -166,11 +161,11 @@ public class Main_UZ extends JFrame {
 		btnLeft3.setForeground(new Color(255, 255, 225));
 		btnLeft3.setBackground(new Color(36, 107, 220));
 		btnLeft3.setFont(new Font("고딕",Font.BOLD,20));
-		jp.add(btnLeft3);
+		this.add(btnLeft3);
 		JPanel panelLeft3 = new JPanel();
 		panelLeft3.setBounds(0, 380, 80, 10);
 		panelLeft3.setBackground(new Color(255, 255, 255));
-		jp.add(panelLeft3);
+		this.add(panelLeft3);
 
 
 		final JButton btnLeft4 = new JButton("<html>단어<br/>TEST</html>");
@@ -178,11 +173,11 @@ public class Main_UZ extends JFrame {
 		btnLeft4.setForeground(new Color(255, 255, 225));
 		btnLeft4.setBackground(new Color(36, 107, 220));
 		btnLeft4.setFont(new Font("고딕",Font.BOLD,20));
-		jp.add(btnLeft4);
+		this.add(btnLeft4);
 		JPanel panelLeft4  = new JPanel();
 		panelLeft4.setBounds(0, 510, 80, 10);
 		panelLeft4.setBackground(new Color(255, 255, 255));
-		jp.add(panelLeft4);
+		this.add(panelLeft4);
 
 
 
@@ -191,15 +186,39 @@ public class Main_UZ extends JFrame {
 		btnLeft5.setForeground(new Color(255, 255, 225));
 		btnLeft5.setBackground(new Color(36, 107, 220));
 		btnLeft5.setFont(new Font("고딕",Font.BOLD,20));
-		jp.add(btnLeft5);
+		this.add(btnLeft5);
 
 
 		
+		
+		btn.addActionListener(new ActionListener() {
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 ChangePanel.changePanel(mf, Main_UZ, new  MyPage_UZ(mf));				
+			}
+			
+		});
+
+		
+		btnLeft5.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, Main_UZ, new Grow_UZ(mf));
+			}
+			
+		});
+		
+		
+
+//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		this.setVisible(true);
 
 	}
+	
+
+	
 }
 
 

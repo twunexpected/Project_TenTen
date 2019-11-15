c04dad0986d9cd1987217f8299d4389265e18149
lucy <lucy@DESKTOP-V1G780L>
2019-11-15 15:02:47 +0900

    다정 GUI완성 내꺼 안에서 연결완료(에러있음)

---
+++ Project_TenTen/src/com/kh/project_TenTen/view/AddWord_DJ.java
@@ -0,0 +1,193 @@
+package com.kh.project_TenTen.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class AddWord_DJ extends JPanel{
+	private MainFrame mf;
+	
+
+	public AddWord_DJ(MainFrame mf) {
+		this.mf = mf;
+	//프레임
+	/*	
+			this.setBounds(650, 100, 400, 700);
+			this.setLayout(null);*/
+			//배경 패널
+			JPanel jp = new JPanel();
+			jp.setSize(400, 700);
+			jp.setLayout(null);
+			jp.setBackground(new Color(123, 185, 237));
+				
+			//단어 추가 라벨
+			JLabel lb3 = new JLabel("단어추가 ");
+			lb3.setHorizontalAlignment(JLabel.CENTER);
+			lb3.setForeground(Color.white);
+			lb3.setLocation(105, 0);
+			lb3.setSize(230, 50);
+			lb3.setFont(new Font("고딕",Font.BOLD,20));
+			lb3.setOpaque(true);
+			lb3.setBackground(new Color(36, 107, 220));
+			jp.add(lb3);
+			//체크리스트
+			JLabel lb4 = new JLabel("주제  ");
+			lb4.setLocation(110, 150);
+			lb4.setSize(120, 50);
+			lb4.setFont(new Font("고딕",Font.BOLD,18));
+			jp.add(lb4);
+			String[] list = {"     여행", "     스포츠", "     요리","     비즈니스","     예술","     경제"};
+			JComboBox mainList = new JComboBox(list);
+			mainList.setSelectedIndex(0);
+			mainList.setLocation(160, 150);
+			mainList.setSize(170, 50);
+			mainList.setFont(new Font("고딕",Font.BOLD,15));
+			jp.add(mainList);
+			//품사
+			JLabel lb5 = new JLabel("품사  ");
+			lb5.setLocation(110, 230);
+			lb5.setSize(120, 50);
+			lb5.setFont(new Font("고딕",Font.BOLD,18));
+			jp.add(lb5);
+			String[] list1 = {"      명사", "      대명사", "      동사","      형용사","      부사","      전치사","      접속사","      감탄사"};
+			JComboBox subList = new JComboBox(list1);
+			subList.setSelectedIndex(0);
+			subList.setLocation(160, 230);
+			subList.setSize(170, 50);
+			subList.setFont(new Font("고딕",Font.BOLD,15));
+			jp.add(subList);
+			//단어
+			JLabel lb1 = new JLabel("단어  ");
+			lb1.setLocation(110, 310);
+			lb1.setSize(120, 50);
+			lb1.setFont(new Font("고딕",Font.BOLD,18));
+			jp.add(lb1);
+			JTextField tf = new JTextField();
+			tf.setLocation(160, 310);
+			tf.setSize(170, 50);
+			jp.add(tf);
+			//뜻
+			JLabel lb2 = new JLabel("뜻  ");
+			lb2.setLocation(120, 390);
+			lb2.setSize(120, 50);
+			lb2.setFont(new Font("고딕",Font.BOLD,18));
+			jp.add(lb2);
+			JTextField tf1 = new JTextField();
+			tf1.setLocation(160, 390);
+			tf1.setSize(170, 50);
+			jp.add(tf1);
+			//등록버튼
+			JButton b1 = new JButton("등록");
+			b1.setHorizontalAlignment(JLabel.CENTER);
+			b1.setForeground(Color.white);
+			b1.setFont(new Font("고딕",Font.BOLD,17));
+			b1.setBackground(new Color(36, 107, 220));
+			b1.setBounds(200, 500, 70, 50);
+			jp.add(b1);
+			
+			b1.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					try{
+						BufferedWriter bo = new BufferedWriter(new FileWriter("단어추가.dat"));
+						
+						bo.write(tf.getText()+"/");
+						bo.write(tf1.getText()+"/\n");
+						
+						bo.close();
+			        } catch (Exception ex) { 
+			      
+			     } 
+
+						 JOptionPane.showMessageDialog(null, "입력하신 단어가 저장되었습니다."); 
+				}
+			});
+			//좌측 고정
+			  final JButton btnLeft1 = new JButton("<html>메인<br/>화면</html>");
+		      btnLeft1.setBounds(0, 0, 80, 120);
+		      btnLeft1.setForeground(new Color(255, 255, 225));
+		      btnLeft1.setBackground(new Color(36, 107, 220));
+		      btnLeft1.setFont(new Font("고딕", Font.BOLD, 20));
+		      jp.add(btnLeft1);
+			  JPanel panelLeft1 = new JPanel();
+		      panelLeft1.setBounds(0,120, 80, 10);
+		      panelLeft1.setBackground(new Color(255, 255, 255));
+		   	  jp.add(panelLeft1);
+		      
+		      final JButton btnLeft2 = new JButton("<html>나의<br/>단어</html>");
+		      btnLeft2.setBounds(0, 130, 80, 120);
+		      btnLeft2.setForeground(new Color(255, 255, 225));
+		      btnLeft2.setBackground(new Color(36, 107, 220));
+		      btnLeft2.setFont(new Font("고딕", Font.BOLD, 20));
+		      jp.add(btnLeft2);
+		      JPanel panelLeft2= new JPanel();
+		      panelLeft2.setBounds(0, 250, 80, 10);
+		      panelLeft2.setBackground(new Color(255, 255, 255));
+		      jp.add(panelLeft2);
+		      btnLeft2.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					ChangePanel.changePanel(mf, jp, new MyWord1_DJ(mf));
+				}
+
+			});
+
+		      
+		      final JButton btnLeft3 = new JButton("<html>단어<br/>추가</html>");
+		      btnLeft3.setBounds(0, 260, 80, 120);
+		      btnLeft3.setForeground(new Color(255, 255, 225));
+		      btnLeft3.setBackground(new Color(36, 107, 220));
+		      btnLeft3.setFont(new Font("고딕", Font.BOLD, 20));
+		      jp.add(btnLeft3);
+		      JPanel panelLeft3 = new JPanel();
+		      panelLeft3.setBounds(0, 380, 80, 10);
+		      panelLeft3.setBackground(new Color(255, 255, 255));
+		      jp.add(panelLeft3);
+		      
+		      final JButton btnLeft4 = new JButton("<html>단어<br/>TEST</html>");
+		      btnLeft4.setBounds(0, 390, 80, 120);
+		      btnLeft4.setForeground(new Color(255, 255, 225));
+		      btnLeft4.setBackground(new Color(36, 107, 220));
+		      btnLeft4.setFont(new Font("고딕", Font.BOLD, 20));
+		      jp.add(btnLeft4);
+		      JPanel panelLeft4  = new JPanel();
+		      panelLeft4.setBounds(0, 510, 80, 10);
+		      panelLeft4.setBackground(new Color(255, 255, 255));
+		      jp.add(panelLeft4);
+		      
+		      final JButton btnLeft5 = new JButton("<html>성장<br/>과정</html>");
+		      btnLeft5.setBounds(0, 520, 80, 135);
+		      btnLeft5.setForeground(new Color(255, 255, 225));
+		      btnLeft5.setBackground(new Color(36, 107, 220));
+		      btnLeft5.setFont(new Font("고딕", Font.BOLD, 20));
+		      jp.add(btnLeft5);
+
+
+			//카피라이터
+			 JLabel naming = new JLabel("Copyrights ⓒ  비!뇨내과 All Rights reserved.");
+		     naming.setBounds(100, 600, 400, 50);
+		     //naming.setForeground(new Color(255, 255, 225));
+		     naming.setFont(new Font("고딕", Font.BOLD, 12));
+		     jp.add(naming);
+			//마무리
+			//this.setVisible(true);
+			mf.add(jp);	
+			/*this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+
+
+		}
+
+
+
+	
+}
---
+++ Project_TenTen/src/com/kh/project_TenTen/view/MyWord1_DJ.java
@@ -0,0 +1,196 @@
+package com.kh.project_TenTen.view;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class MyWord1_DJ extends JPanel{
+	private MainFrame mf;
+	
+	public MyWord1_DJ(MainFrame mf) {
+this.mf = mf;
+
+		//프레임
+		this.setSize(400, 700);
+		this.setLocation(650,100);
+		//기본패널
+		JPanel myWord1 = new JPanel();
+		myWord1.setLayout(null);
+		myWord1.setBackground(new Color(123, 185, 237));
+		myWord1.setSize(400,700);
+		//표올릴 패널
+		JPanel p = new JPanel();
+		p.setSize(210,490);
+		p.setLocation(130, 100);
+		//표
+		String[] headings = new String[] {"번호","단어","뜻"};
+		Object data[][] ={
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+				{"1","apple","사과"},
+				{"2","banana","바나나"},
+				{"3","orange","오렌지"},
+		};
+		JTable table = new JTable(data,headings);
+		table.setPreferredScrollableViewportSize(new Dimension(190,470));
+		table.setFillsViewportHeight(true);
+		p.add(new JScrollPane(table));
+		//암기 비암기 버튼
+		JButton b1 = new JButton("암기");
+		b1.setHorizontalAlignment(JLabel.CENTER);
+		b1.setForeground(Color.white);
+		b1.setFont(new Font("고딕",Font.BOLD,17));
+		b1.setBackground(new Color(225, 91, 91));
+		JButton b2 = new JButton("비암기");
+		b2.setHorizontalAlignment(JLabel.CENTER);
+		b2.setForeground(Color.white);
+		b2.setFont(new Font("고딕",Font.BOLD,14));
+		b2.setBackground(new Color(36, 107, 220));
+		b2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, myWord1, new MyWord2_DJ(mf));
+			}
+
+		});
+		JButton b3 = new JButton("즐겨찾기");
+		b3.setHorizontalAlignment(JLabel.CENTER);
+		b3.setForeground(Color.white);
+		b3.setFont(new Font("고딕",Font.BOLD,12));
+		b3.setBackground(new Color(36, 107, 220));
+		b1.setBounds(95, 20, 70, 50);
+		b2.setBounds(180, 20, 79, 50);
+		b3.setBounds(275, 20, 87, 50);	
+		b3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, myWord1, new MyWord3_DJ(mf));
+			}
+
+		});
+		myWord1.add(b1);
+		myWord1.add(b2);
+		myWord1.add(b3);
+		myWord1.add(p);
+		//좌측 고정
+		final JButton btnLeft1 = new JButton("<html>메인<br/>화면</html>");
+		btnLeft1.setBounds(0, 0, 80, 120);
+		btnLeft1.setForeground(new Color(255, 255, 225));
+		btnLeft1.setBackground(new Color(36, 107, 220));
+		btnLeft1.setFont(new Font("고딕", Font.BOLD, 20));
+		myWord1.add(btnLeft1);
+		JPanel panelLeft1 = new JPanel();
+		panelLeft1.setBounds(0,120, 80, 10);
+		panelLeft1.setBackground(new Color(255, 255, 255));
+		myWord1.add(panelLeft1);
+
+		final JButton btnLeft2 = new JButton("<html>나의<br/>단어</html>");
+		btnLeft2.setBounds(0, 130, 80, 120);
+		btnLeft2.setForeground(new Color(255, 255, 225));
+		btnLeft2.setBackground(new Color(36, 107, 220));
+		btnLeft2.setFont(new Font("고딕", Font.BOLD, 20));
+		myWord1.add(btnLeft2);
+		JPanel panelLeft2= new JPanel();
+		panelLeft2.setBounds(0, 250, 80, 10);
+		panelLeft2.setBackground(new Color(255, 255, 255));
+		myWord1.add(panelLeft2);
+
+		final JButton btnLeft3 = new JButton("<html>단어<br/>추가</html>");
+		btnLeft3.setBounds(0, 260, 80, 120);
+		btnLeft3.setForeground(new Color(255, 255, 225));
+		btnLeft3.setBackground(new Color(36, 107, 220));
+		btnLeft3.setFont(new Font("고딕", Font.BOLD, 20));
+		myWord1.add(btnLeft3);
+		JPanel panelLeft3 = new JPanel();
+		panelLeft3.setBounds(0, 380, 80, 10);
+		panelLeft3.setBackground(new Color(255, 255, 255));
+		myWord1.add(panelLeft3);
+		btnLeft3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, myWord1, new AddWord_DJ(mf));
+			}
+
+		});
+
+		final JButton btnLeft4 = new JButton("<html>단어<br/>TEST</html>");
+		btnLeft4.setBounds(0, 390, 80, 120);
+		btnLeft4.setForeground(new Color(255, 255, 225));
+		btnLeft4.setBackground(new Color(36, 107, 220));
+		btnLeft4.setFont(new Font("고딕", Font.BOLD, 20));
+		myWord1.add(btnLeft4);
+		JPanel panelLeft4  = new JPanel();
+		panelLeft4.setBounds(0, 510, 80, 10);
+		panelLeft4.setBackground(new Color(255, 255, 255));
+		myWord1.add(panelLeft4);
+
+		final JButton btnLeft5 = new JButton("<html>성장<br/>과정</html>");
+		btnLeft5.setBounds(0, 520, 80, 135);
+		btnLeft5.setForeground(new Color(255, 255, 225));
+		btnLeft5.setBackground(new Color(36, 107, 220));
+		btnLeft5.setFont(new Font("고딕", Font.BOLD, 20));
+		myWord1.add(btnLeft5);
+
+
+		//카피라이터
+		JLabel naming = new JLabel("Copyrights ⓒ  비!뇨내과 All Rights reserved.");
+		naming.setBounds(100, 600, 400, 50);
+		//naming.setForeground(new Color(255, 255, 225));
+		naming.setFont(new Font("고딕", Font.BOLD, 12));
+		this.add(naming);
+		mf.add(myWord1);
+	
+
+	}	
+
+}
+
+
+

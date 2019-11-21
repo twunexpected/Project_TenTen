b77bfef6d78e706ca39f26447354572283dae742
apple <apple@218.38.137.28>
2019-11-21 17:10:28 +0900

    Test 수

--- Project_TenTen/src/com/kh/project_TenTen/view/Test_Main.java
+++ Project_TenTen/src/com/kh/project_TenTen/view/Test_Main.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,37 +16,59 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
+import com.kh.project_TenTen.model.dao.WordDao;
+import com.kh.project_TenTen.model.vo.Word;
 
 public class Test_Main extends JPanel {
 
 	private Login_MainFrame mf;
 	private JPanel Test_Main;
 
+	WordDao wd = new WordDao();
+	String[] spellStr = null;	//단어 스펠링
+	String[] meanStr = null;	//뜻
+	String[] checkStr = null;	// 정답비교
+
+	public static Word[] word = null;
+	
+	//int MainIndex;
+
+	char [] checkAns = null;
+	
+	int index = 0;
+	int answer = 0;
+	int count = 0;
+	
+	
+	public int returnSubject(String returnSubject) {
+		int num = 0;
+		
+		switch(returnSubject) {
+		case "요리" : break;
+		case "스포츠" : num = 1; break;
+		case "여행" : num = 2; break;
+		case "회사" : num = 3; break;
+		case "경제" : num = 4; break;
+		case "예술" : num = 5; break;
+		}
+		
+		return num;
+	}
+	
+	
+	
+	
 	public Test_Main(Login_MainFrame mf) {
 		this.mf = mf;
 		Test_Main = this;
 
+		
 
-		//this.setBounds(650, 100, 400, 700);
-		//this.setLayout(null);
+		
 		//백 화면
 		this.setSize(400, 700);
 		this.setBackground(new Color(123, 185, 237));
 		this.setLayout(null);
-
-		//		//상단 패널
-		//		JPanel panelTitle = new JPanel();
-		//		panelTitle.setBounds(130, 0, 200, 50);
-		//		panelTitle.setBackground(new Color(36, 107, 220));
-		//		this.setLayout(null);
-		//
-		//		//상단 박스
-		//		JLabel lableTitle = new JLabel("단어 Test");
-		//		lableTitle.setBounds(180, 0, 100, 50);
-		//		lableTitle.setForeground(new Color(255, 255, 225));
-		//		lableTitle.setFont(new Font("고딕", Font.BOLD, 24));
-		//		this.setLayout(null);
 
 		//단어 추가 라벨
 		JLabel panelTitle = new JLabel("단어 Test");
@@ -56,22 +81,21 @@ public class Test_Main extends JPanel {
 		panelTitle.setBackground(new Color(36, 107, 220));
 
 		//본문
-		JLabel labelCenter = new JLabel("Test 조건을 충족하였습니다.");
-		labelCenter.setLocation(125, 220);
+		JLabel labelCenter = new JLabel("Test 시작버튼을 눌러주세요.");
+		labelCenter.setLocation(140, 220);
 		labelCenter.setSize(220, 100);
 		labelCenter.setFont(new Font("고딕", Font.BOLD, 16));
 		this.setLayout(null);
 
-
-		JButton btnCenter = new JButton("TEST 시작");
-		btnCenter.setBounds(150, 300, 150, 50);
+		//Test 시작 버튼
+		JButton btnCenter = new JButton("Test 시작");
+		btnCenter.setBounds(155, 300, 150, 50);
 		btnCenter.setForeground(new Color(255, 255, 225));
 		btnCenter.setBackground(new Color(36, 107, 220));
 		btnCenter.setFont(new Font("고딕", Font.BOLD, 20));
 		this.setLayout(null);
 
 		//좌측 
-
 		JPanel panelLeft1 = new JPanel();
 		panelLeft1.setBounds(0,120, 80, 10);
 		panelLeft1.setBackground(new Color(255, 255, 255));
@@ -135,13 +159,28 @@ public class Test_Main extends JPanel {
 		this.setLayout(null);
 		this.add(naming);
 
+		
+		
+		
+		//********* To do ********
+		//메인에서 10개 단어 넘기면 버튼클릭 가능하게 아니면 다이얼로그 메시지 경고창 
+		
 		btnCenter.addActionListener(new ActionListener() {//익명
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				ChangePanel.changePanel(mf, Test_Main, new Test_ChooseTest(mf));
+				if(MainPage.MainIndex == 10) {
+
+					ChangePanel.changePanel(mf, Test_Main, new Test_ChooseTest(mf));
+				} else {
+					
+					JOptionPane.showMessageDialog(null, "먼저 오늘의 단어학습을 완료해주세요.");
+				}
+				
 			}
 		});
 
+		
+		
 
 		btnLeft1.addActionListener(new ActionListener() {
 
@@ -188,8 +227,6 @@ public class Test_Main extends JPanel {
 			}
 
 		});
-
-
 
 
 
